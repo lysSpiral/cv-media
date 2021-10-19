cdb9481330a98d1e24808346033f679a9477c607
Lys Spiral <lys.gass@gmail.com>
2021-10-20 01:22:25 +0200

    Création du dépôt / Ajout des fichiers au 19 oct 2021

---
+++ docs/js/navbar_script.js
@@ -0,0 +1,25 @@
+console.log('Connexion js établie');
+
+const btnRespMenu = document.querySelector('.btn-responsive-menu');
+const listeMenu = document.querySelector('.liste-nav');
+const rubMenu = document.querySelectorAll('.item');
+const bgMenu = document.getElementById('nav');
+
+function menuResp() {
+	btnRespMenu.classList.toggle('active');
+	console.log('Clic sur le bouton');
+	listeMenu.classList.toggle('active-menu');
+	bgMenu.classList.toggle('active');
+
+}
+
+btnRespMenu.addEventListener('click',()=>{
+	menuResp();
+});
+
+rubMenu.forEach(item => {
+	(item.addEventListener('click',()=>{
+	menuResp();
+	})
+	);
+})
