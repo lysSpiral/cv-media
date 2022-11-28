a79ce83dad59148afefde897cbd5b6a5187e6fec
Lys Spiral <lys.gass@gmail.com>
2022-11-28 17:26:28 +0100

    Mise a jour Nov 2022 - travail sur liens vers sections collapsible

--- docs/js/general_script.js
+++ docs/js/general_script.js
@@ -1,4 +1,7 @@
 console.log('Connexion js établie - general script');
+openCollapse();
+
+
 
 function switchImg(imgId){
 	var img = document.getElementById(imgId);
@@ -7,3 +10,17 @@ function switchImg(imgId){
 	// console.log('new : '+img.getAttribute('src'));
 	//console.log('switchImg');
 }
+
+function openCollapse() {
+	document.addEventListener("DOMContentLoaded", function () {
+		//console.log("openCollapseVar");
+		//console.log(location.hash);
+  	location.hash && $('#collapse'+location.hash.substring(1)+ '.collapse').collapse('show');
+  	//console.log('plus'+location.hash.substring(9))
+	 (document.getElementById('plus'+location.hash.substring(1)) !== null) ?   	document.getElementById('plus'+location.hash.substring(1)).src = 'img/minus.png' : console.log("pas de signe");
+  	
+	});
+}
+
+
+
