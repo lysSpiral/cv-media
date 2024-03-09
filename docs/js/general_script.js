c16b4f96fd404061307217529da3d73f11e131dd
LysGass <lys.gass@gmail.com>
2024-03-09 23:10:43 +0100

    MaJ S2 2023-T1 2024

--- docs/js/general_script.js
+++ docs/js/general_script.js
@@ -3,6 +3,7 @@ openCollapse();
 
 
 
+
 function switchImg(imgId){
 	var img = document.getElementById(imgId);
 	//console.log('previous : '+img.getAttribute('src'));
@@ -13,14 +14,40 @@ function switchImg(imgId){
 
 function openCollapse() {
 	document.addEventListener("DOMContentLoaded", function () {
-		//console.log("openCollapseVar");
-		//console.log(location.hash);
+		console.log("openCollapseVar");
+		console.log(location.hash);
   	location.hash && $('#collapse'+location.hash.substring(1)+ '.collapse').collapse('show');
-  	//console.log('plus'+location.hash.substring(9))
+  	console.log('plus'+location.hash.substring(9));
 	 (document.getElementById('plus'+location.hash.substring(1)) !== null) ?   	document.getElementById('plus'+location.hash.substring(1)).src = 'img/minus.png' : console.log("pas de signe");
   	
 	});
 }
 
+function closeCollapseAll() {
+	var collapseList=document.querySelectorAll('.collapse');
+	console.log(collapseList.length);
+	collapseList.forEach (function(element) {
+			console.log(element.id);
+			console.log(element.id.substring(8));
+			console.log('plus'+element.id.substring(8));
+			$('#'+element.id+'.collapse').collapse('hide');
+			document.getElementById('plus'+element.id.substring(8)).src = 'img/plus.png';
+		}
+		);
+	//$('#collapse'+'LecturePub'+'.collapse').collapse('hide')
+}
+
+function openCollapseSamePage(sectionId) {
+	// FERMER TOUTES LES SECTIONS
+	closeCollapseAll();
+
+	// OUVRIR LA SECTION PASSEE EN PARAMETRE
+	$('#collapse'+sectionId.substring(1)+ '.collapse').collapse('show');
+  	console.log(sectionId);
+  	console.log('plus'+sectionId.substring(1));
+  	 (document.getElementById('plus'+sectionId.substring(1)) !== null) ?   	document.getElementById('plus'+sectionId.substring(1)).src = 'img/minus.png' : console.log("pas de signe");
+	 
+  	
+}
 
 
