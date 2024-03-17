736d577c6bb5616bbb87238f01f866787a6814a3
LysGass <lys.gass@gmail.com>
2024-03-17 14:56:58 +0100

    Modification navigation collapse - Erreur orthographe ponctuation - modif lien journal 10

--- docs/js/general_script.js
+++ docs/js/general_script.js
@@ -2,22 +2,23 @@ console.log('Connexion js établie - general script');
 openCollapse();
 
 
-
-
 function switchImg(imgId){
 	var img = document.getElementById(imgId);
 	//console.log('previous : '+img.getAttribute('src'));
 	img.src = (img.src.match('img/plus.png')) ? 'img/minus.png' : 'img/plus.png';
-	// console.log('new : '+img.getAttribute('src'));
-	//console.log('switchImg');
+}
+
+function switchTxt(txtId, txt1, txt2){
+	var txt = document.getElementById(txtId);
+	txt.innerHTML = (txt.innerHTML.match(txt1)) ? txt2 : txt1;
 }
 
 function openCollapse() {
 	document.addEventListener("DOMContentLoaded", function () {
 		console.log("openCollapseVar");
-		console.log(location.hash);
-  	location.hash && $('#collapse'+location.hash.substring(1)+ '.collapse').collapse('show');
-  	console.log('plus'+location.hash.substring(9));
+		console.log(location.hash, '#collapse'+location.hash.substring(1)+ '.collapse');
+ 	location.hash && $('#collapse'+location.hash.substring(1)+ '.collapse').collapse('show');
+  	console.log('plus'+location.hash.substring(1));
 	 (document.getElementById('plus'+location.hash.substring(1)) !== null) ?   	document.getElementById('plus'+location.hash.substring(1)).src = 'img/minus.png' : console.log("pas de signe");
   	
 	});
@@ -25,29 +26,38 @@ function openCollapse() {
 
 function closeCollapseAll() {
 	var collapseList=document.querySelectorAll('.collapse');
-	console.log(collapseList.length);
+	//console.log(collapseList.length);
 	collapseList.forEach (function(element) {
-			console.log(element.id);
-			console.log(element.id.substring(8));
-			console.log('plus'+element.id.substring(8));
 			$('#'+element.id+'.collapse').collapse('hide');
 			document.getElementById('plus'+element.id.substring(8)).src = 'img/plus.png';
 		}
 		);
-	//$('#collapse'+'LecturePub'+'.collapse').collapse('hide')
+}
+
+function closeCollapseAllButOne(sectionId) {
+	var collapseList=document.querySelectorAll('.collapse');
+	//console.log(collapseList.length);
+	collapseList.forEach (function(element) {
+		if (element.id !== sectionId) {
+			$('#'+element.id+'.collapse').collapse('hide');
+			document.getElementById('plus'+element.id.substring(8)).src = 'img/plus.png';
+		}	
+	}
+	);
+}
+
+function openCollapseSamePageSub (sectionId) {
+	 $('#collapse'+sectionId.substring(1)+ '.collapse').collapse('show');
+  	 (document.getElementById('plus'+sectionId.substring(1)) !== null) ?   	document.getElementById('plus'+sectionId.substring(1)).src = 'img/minus.png' : console.log("pas de signe");
+
 }
 
 function openCollapseSamePage(sectionId) {
-	// FERMER TOUTES LES SECTIONS
-	closeCollapseAll();
-
-	// OUVRIR LA SECTION PASSEE EN PARAMETRE
-	$('#collapse'+sectionId.substring(1)+ '.collapse').collapse('show');
-  	console.log(sectionId);
-  	console.log('plus'+sectionId.substring(1));
-  	 (document.getElementById('plus'+sectionId.substring(1)) !== null) ?   	document.getElementById('plus'+sectionId.substring(1)).src = 'img/minus.png' : console.log("pas de signe");
-	 
-  	
+	// FERMER LES AUTRES SECTIONS
+	var savedFromCollapseID = 'collapse'+sectionId.substring(1);
+	closeCollapseAllButOne(savedFromCollapseID);
+ 	
+	openCollapseSamePageSub(sectionId);
 }
 
 
